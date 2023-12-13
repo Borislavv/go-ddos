708ba4f957f35343bce00c738e240910a4c904c5
Boris <Glazunov2142@icloud.com>
2023-12-13 12:00:00 +0300

    chore: refactored display/app/display.

--- internal/display/app/display.go
+++ internal/display/app/display.go
@@ -1,33 +1,22 @@
-package app
+package display
 
 import (
 	"context"
-	"fmt"
+	model "ddos/internal/display/domain/model"
 	"github.com/nsf/termbox-go"
 	"github.com/olekukonko/tablewriter"
 	"log"
 	"os"
 	"sync"
-	"time"
 )
-
-type Data struct {
-	CurrentDuration        time.Duration
-	TargetRPC              int
-	CurrentRPC             int
-	CurrentWorkers         int64
-	CurrentTotalRequests   int64
-	CurrentFailedRequests  int64
-	CurrentSuccessRequests int64
-}
 
 type Display struct {
 	ctx    context.Context
-	dataCh chan *Data
+	dataCh chan *model.Table
 }
 
-func NewDisplay(ctx context.Context, dataCh chan *Data) *Display {
-	return &Display{ctx: ctx, dataCh: dataCh}
+func New(ctx context.Context) *Display {
+	return &Display{ctx: ctx, dataCh: make(chan *model.Table, 1000)}
 }
 
 func (d *Display) Run(mwg *sync.WaitGroup) {
@@ -40,74 +29,66 @@ func (d *Display) Run(mwg *sync.WaitGroup) {
 	defer termbox.Close()
 
 	wg := &sync.WaitGroup{}
-	wg.Add(1)
+	wg.Add(2)
 	go d.draw(wg)
+	go d.close(wg)
 	wg.Wait()
+}
+
+func (d *Display) Draw(t *model.Table) {
+	d.dataCh <- t
+}
+
+func (d *Display) close(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		select {
+		case <-d.ctx.Done():
+			return
+		default:
+			switch ev := termbox.PollEvent(); ev.Type {
+			case termbox.EventKey:
+				if ev.Key == termbox.KeyCtrlC || ev.Key == termbox.KeyCtrlZ {
+					termbox.Close()
+					return
+				}
+			case termbox.EventError:
+				panic(ev.Err)
+			}
+		}
+	}
 }
 
 func (d *Display) draw(wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"duration", "rpc", "workers", "total reqs.", "success reqs.", "failed reqs."})
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case <-d.ctx.Done():
-				return
-			default:
-				switch ev := termbox.PollEvent(); ev.Type {
-				case termbox.EventKey:
-					if ev.Key == termbox.KeyCtrlC || ev.Key == termbox.KeyCtrlZ {
-						termbox.Close()
-						return
-					}
-				case termbox.EventError:
-					panic(ev.Err)
-				}
+	for {
+		select {
+		case <-d.ctx.Done():
+			termbox.Close()
+			return
+		case data := <-d.dataCh:
+			if err := termbox.Clear(termbox.ColorDefault, termbox.ColorDefault); err != nil {
+				log.Fatalln(err)
+			}
+			if err := termbox.Sync(); err != nil {
+				log.Fatalln(err)
+			}
+
+			table.SetHeader(data.Header)
+			table.ClearRows()
+
+			for _, row := range data.Rows {
+				table.Append(row)
+			}
+
+			table.Render()
+			if err := termbox.Flush(); err != nil {
+				log.Fatalln(err)
 			}
 		}
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case <-d.ctx.Done():
-				termbox.Close()
-				return
-			case data := <-d.dataCh:
-				if err := termbox.Clear(termbox.ColorDefault, termbox.ColorDefault); err != nil {
-					log.Fatalln(err)
-				}
-				if err := termbox.Sync(); err != nil {
-					log.Fatalln(err)
-				}
-
-				// Очиста старых строк
-				table.ClearRows()
-				// Добавление данных в таблицу
-				table.Append(
-					[]string{
-						data.CurrentDuration.String(),
-						fmt.Sprintf("%d", data.CurrentRPC),
-						fmt.Sprintf("%d", data.CurrentWorkers),
-						fmt.Sprintf("%d", data.CurrentTotalRequests),
-						fmt.Sprintf("%d", data.CurrentSuccessRequests),
-						fmt.Sprintf("%d", data.CurrentFailedRequests),
-					},
-				)
-				// Отрисовка таблицы
-				table.Render()
-
-				if err := termbox.Flush(); err != nil {
-					log.Fatalln(err)
-				}
-			}
-		}
-	}()
+	}
 }
