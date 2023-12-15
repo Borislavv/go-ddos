0501123a55613f0ce27f0f1054532837a891c385
Boris <Glazunov2142@icloud.com>
2023-12-15 17:56:19 +0300

    chore: init config.

---
+++ internal/ddos/app/config.go
@@ -0,0 +1,7 @@
+package app
+
+type Config struct {
+	RPC      int    `env:"RPC" envDefault:"100"`
+	Workers  int    `env:"WORKERS" envDefault:"10"`
+	Duration string `env:"DURATION" envDefault:"15m"`
+}
