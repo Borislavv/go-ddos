2f7d3e5e64f422342819f51be914ddefdb5c8a33
Boris <Glazunov2142@icloud.com>
2023-12-15 17:57:02 +0300

    chore: main.

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,35 @@
-package cmd
+package main
+
+import (
+	"context"
+	"ddos/internal/ddos/app"
+	app2 "ddos/internal/display/app"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+func main() {
+	exit := make(chan os.Signal, 1)
+	defer close(exit)
+	signal.Notify(exit, os.Interrupt, syscall.SIGTERM)
+
+	wg := &sync.WaitGroup{}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer wg.Wait()
+
+	dataCh := make(chan *app2.Data, 1000)
+
+	wg.Add(2)
+	go app.NewDDOS(ctx, dataCh).Run(wg)
+	go app2.NewDisplay(ctx, dataCh).Run(wg)
+
+	log.Println("awaiting ctrl+c")
+	<-exit
+	log.Println("received ctrl+c sing, closing...")
+	cancel()
+	wg.Wait()
+	log.Println("exited!")
+}
