29207deb066b373bcac00b5187709c25996854e6
Boris <Glazunov2142@icloud.com>
2023-12-16 15:45:03 +0300

    chore: displayer separater to renderer service.

--- internal/display/app/display.go
+++ internal/display/app/display.go
@@ -3,129 +3,37 @@ package display
 import (
 	"context"
 	model "ddos/internal/display/domain/model"
-	"github.com/nsf/termbox-go"
-	"github.com/olekukonko/tablewriter"
-	"log"
+	displayservice "ddos/internal/display/domain/service"
 	"os"
 	"sync"
 )
 
 type Display struct {
-	mu     *sync.Mutex
 	ctx    context.Context
-	exit   chan os.Signal
 	dataCh chan *model.Table
-	table  *tablewriter.Table
-	stopCh chan struct{}
+	exitCh chan os.Signal
 }
 
-func New(ctx context.Context, exit chan os.Signal) *Display {
+func New(ctx context.Context, exitCh chan os.Signal) *Display {
 	return &Display{
-		mu:     &sync.Mutex{},
 		ctx:    ctx,
-		exit:   exit,
+		exitCh: exitCh,
 		dataCh: make(chan *model.Table, 1000),
-		stopCh: make(chan struct{}, 1),
 	}
 }
 
 func (d *Display) Run(mwg *sync.WaitGroup) {
 	defer mwg.Done()
 
-	err := termbox.Init()
-	if err != nil {
-		panic(err)
-	}
-	defer termbox.Close()
-
-	d.table = tablewriter.NewWriter(os.Stdout)
+	renderer := displayservice.NewRenderer(d.ctx, d.exitCh, d.dataCh)
 
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
-	go d.draw(wg)
-	go d.close(wg)
+	go renderer.Draw(wg)
+	go renderer.Close(wg)
 	wg.Wait()
 }
 
 func (d *Display) Draw(t *model.Table) {
 	d.dataCh <- t
-}
-
-func (d *Display) close(wg *sync.WaitGroup) {
-	defer func() {
-		close(d.stopCh)
-		wg.Done()
-	}()
-
-	for {
-		select {
-		case <-d.ctx.Done():
-			return
-		default:
-			switch ev := termbox.PollEvent(); ev.Type {
-			case termbox.EventKey:
-				if ev.Key == termbox.KeyCtrlC || ev.Key == termbox.KeyCtrlZ {
-					return
-				}
-			case termbox.EventError:
-				panic(ev.Err)
-			}
-		}
-	}
-}
-
-func (d *Display) draw(wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	for {
-		select {
-		case <-d.stopCh:
-			termbox.Close()
-
-			d.exit <- os.Interrupt
-
-			// draw the summary table
-			table := tablewriter.NewWriter(os.Stdout)
-			table.SetHeader([]string{"hello"})
-			table.Append([]string{"world"})
-
-			// render the summary table
-			table.Render()
-
-			// draw the summary table
-			if err := termbox.Flush(); err != nil {
-				log.Fatalln(err)
-			}
-
-			return
-		case data := <-d.dataCh:
-			// clear a terminal window
-			if err := termbox.Clear(termbox.ColorDefault, termbox.ColorDefault); err != nil {
-				log.Fatalln(err)
-			}
-			// sync a terminal window
-			if err := termbox.Sync(); err != nil {
-				return
-			}
-
-			// set up a table header
-			d.table.SetHeader(data.Header)
-
-			// clear a table rows
-			d.table.ClearRows()
-
-			// set up a table rows
-			for _, row := range data.Rows {
-				d.table.Append(row)
-			}
-
-			// render a table
-			d.table.Render()
-
-			// draw a table
-			if err := termbox.Flush(); err != nil {
-				log.Fatalln(err)
-			}
-		}
-	}
 }
