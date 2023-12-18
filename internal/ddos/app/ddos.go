24816045051095b1b49b2e812f098a48cc3ab9b0
Boris <Glazunov2142@icloud.com>
2023-12-18 10:44:28 +0300

    chore: removed unnecessary code from ddos.

--- internal/ddos/app/ddos.go
+++ internal/ddos/app/ddos.go
@@ -6,9 +6,7 @@ import (
 	service "ddos/internal/ddos/domain/service"
 	display "ddos/internal/display/app"
 	statservice "ddos/internal/stat/domain/service"
-	"log"
 	"sync"
-	"time"
 )
 
 type DDOS struct {
@@ -30,24 +28,7 @@ func New(ctx context.Context, cfg *config.Config, display *display.Display, coll
 func (app *DDOS) Run(mwg *sync.WaitGroup) {
 	defer mwg.Done()
 
-	ctx, cancel, err := app.initCtx()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer cancel()
-
-	f := service.NewFlooder(ctx, app.cfg, app.display, app.collector)
+	f := service.NewFlooder(app.ctx, app.cfg, app.display, app.collector)
 
 	f.Run()
-}
-
-func (app *DDOS) initCtx() (context.Context, context.CancelFunc, error) {
-	duration, err := time.ParseDuration(app.cfg.Duration)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	ctx, cancel := context.WithTimeout(app.ctx, duration)
-
-	return ctx, cancel, nil
 }
