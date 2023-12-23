0028e342a3bcf6e0f81372636440d7724b128e06
Boris <Glazunov2142@icloud.com>
2023-12-23 08:27:02 +0300

    chore: improved config.

--- config/config.go
+++ config/config.go
@@ -1,8 +1,9 @@
 package config
 
 type Config struct {
-	MaxRPS      int    `arg:"-r,env:MAX_RPS"         default:"60"`
+	MaxRPS      int    `arg:"-r,env:MAX_RPS"         default:"10"`
 	Percentiles int    `arg:"-p,env:NUM_PERCENTILES" default:"4"`
-	MaxWorkers  int64  `arg:"-w,env:MAX_WORKERS"     default:"10"`
+	MaxWorkers  int64  `arg:"-w,env:MAX_WORKERS"     default:"5"`
 	Duration    string `arg:"-d,env:DURATION"        default:"10m"`
+	URL         string `arg:"-u,env:URL,required"`
 }
