2d6827107d2827f73914e507885c61fd14f29256
Boris <Glazunov2142@icloud.com>
2023-12-24 12:17:04 +0300

    chore: collector improved.

--- internal/stat/domain/service/collector.go
+++ internal/stat/domain/service/collector.go
@@ -3,15 +3,18 @@ package statservice
 import (
 	"ddos/config"
 	"ddos/internal/stat/domain/model"
+	"log"
+	"math"
 	"sync"
 	"sync/atomic"
 	"time"
 )
 
 type Collector struct {
-	mu          *sync.RWMutex
-	cfg         *config.Config
-	percentiles map[int]*model.Metrics
+	mu                 *sync.RWMutex
+	cfg                *config.Config
+	durPerPercentile   time.Duration
+	percentilesMetrics map[int64]*model.Metrics
 
 	// time
 	startedAt time.Time
@@ -29,123 +32,315 @@ type Collector struct {
 }
 
 func NewCollector(cfg *config.Config) *Collector {
+	dur, err := time.ParseDuration(cfg.Duration)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	c := &Collector{
-		mu:  &sync.RWMutex{},
-		cfg: cfg,
+		mu:               &sync.RWMutex{},
+		cfg:              cfg,
+		durPerPercentile: time.Duration(math.Ceil(float64(dur.Nanoseconds() / cfg.Percentiles))),
 	}
-	if cfg.Percentiles > 0 {
-		c.percentiles = make(map[int]*model.Metrics, cfg.Percentiles)
+
+	if cfg.Percentiles <= 0 {
+		atomic.CompareAndSwapInt64(&cfg.Percentiles, cfg.Percentiles, 1)
 	}
+
+	c.mu.Lock()
+	c.percentilesMetrics = make(map[int64]*model.Metrics, cfg.Percentiles)
+	c.mu.Unlock()
+
 	return c
 }
 
-func (c *Collector) SetStartedAt(s time.Time) {
+func (c *Collector) CurrentPercentile() int64 {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	c.startedAt = s
+	return c.currentPercentile()
 }
 
-func (c *Collector) StartedAt() time.Time {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	return c.startedAt
+func (c *Collector) currentPercentile() int64 {
+	return int64(
+		math.Round(float64(time.Since(c.firstMetric().StartedAt()).Milliseconds()/c.durPerPercentile.Milliseconds())),
+	) + 1
 }
 
-func (c *Collector) Duration() time.Duration {
+func (c *Collector) firstPercentile() int64 {
+	return 1
+}
+
+func (c *Collector) currentMetric() *model.Metrics {
+	metric, ok := c.percentilesMetrics[c.currentPercentile()]
+	if !ok {
+		if prevMetric, isset := c.percentilesMetrics[c.currentPercentile()-1]; isset {
+			prevMetric.Lock()
+		}
+		metric = model.NewMetric()
+		c.percentilesMetrics[c.currentPercentile()] = metric
+	}
+	return metric
+}
+
+func (c *Collector) firstMetric() *model.Metrics {
+	metric, ok := c.percentilesMetrics[c.firstPercentile()]
+	if !ok {
+		metric = model.NewMetric()
+		c.percentilesMetrics[c.firstPercentile()] = metric
+	}
+	return metric
+}
+
+// Percentiles number. This value is not mutable.
+func (c *Collector) Percentiles() int64 {
+	return c.cfg.Percentiles
+}
+
+func (c *Collector) Metric(percentile int64) (metric *model.Metrics, found bool) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
-	return time.Since(c.startedAt)
+	metric, found = c.percentilesMetrics[percentile]
+	return metric, found
+}
+
+func (c *Collector) SummaryDuration() time.Duration {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return time.Since(c.firstMetric().StartedAt())
 }
 
 func (c *Collector) SetRPS() {
-	atomic.CompareAndSwapInt64(
-		&c.rps, atomic.LoadInt64(&c.rps), int64(float64(c.Total())/time.Since(c.StartedAt()).Seconds()),
-	)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.currentMetric().SetRPS()
 }
 
 func (c *Collector) RPS() int64 {
-	return atomic.LoadInt64(&c.rps)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.currentMetric().RPS()
+}
+
+func (c *Collector) SummaryRPS() int64 {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return int64(float64(c.summaryTotal()) / time.Since(c.firstMetric().StartedAt()).Seconds())
 }
 
 func (c *Collector) AddWorker() {
-	atomic.AddInt64(&c.workers, 1)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.currentMetric().AddWorker()
 }
 
 func (c *Collector) Workers() int64 {
-	return atomic.LoadInt64(&c.workers)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.currentMetric().Workers()
 }
 
 func (c *Collector) AddTotal() {
-	atomic.AddInt64(&c.total, 1)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.currentMetric().AddTotal()
 }
 
 func (c *Collector) Total() int64 {
-	return atomic.LoadInt64(&c.total)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.currentMetric().Total()
+}
+
+func (c *Collector) summaryTotal() int64 {
+	var t int64
+	for _, metric := range c.percentilesMetrics {
+		t += metric.Total()
+	}
+	return t
+}
+
+func (c *Collector) SummaryTotal() int64 {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	return c.summaryTotal()
 }
 
 func (c *Collector) AddSuccess() {
-	atomic.AddInt64(&c.success, 1)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.currentMetric().AddSuccess()
 }
 
 func (c *Collector) Success() int64 {
-	return atomic.LoadInt64(&c.success)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.currentMetric().Success()
+}
+
+func (c *Collector) SummarySuccess() int64 {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	var t int64
+	for _, metric := range c.percentilesMetrics {
+		t += metric.Success()
+	}
+	return t
 }
 
 func (c *Collector) AddFailed() {
-	atomic.AddInt64(&c.failed, 1)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.currentMetric().AddFailed()
 }
 
 func (c *Collector) Failed() int64 {
-	return atomic.LoadInt64(&c.failed)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.currentMetric().Failed()
 }
 
+func (c *Collector) SummaryFailed() int64 {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	var t int64
+	for _, metric := range c.percentilesMetrics {
+		t += metric.Failed()
+	}
+	return t
+}
+
+// AddTotalDuration for current percentile.
 func (c *Collector) AddTotalDuration(d time.Duration) {
-	atomic.AddInt64(&c.totalDuration, d.Milliseconds())
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.currentMetric().AddTotalDuration(d)
 }
 
-func (c *Collector) TotalDuration() (ms int64) {
-	return atomic.LoadInt64(&c.totalDuration)
-}
-
+// AvgTotalDuration of current percentile.
 func (c *Collector) AvgTotalDuration() time.Duration {
-	total := atomic.LoadInt64(&c.total)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.currentMetric().AvgTotalDuration()
+}
+
+// SummaryAvgTotalDuration of all percentiles.
+func (c *Collector) SummaryAvgTotalDuration() time.Duration {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	var total int64
+	var totalDuration int64
+	for _, metric := range c.percentilesMetrics {
+		total += metric.Total()
+		totalDuration += metric.TotalDuration()
+	}
+
 	if total == 0 {
 		return time.Duration(0)
 	} else {
-		return time.Duration(atomic.LoadInt64(&c.totalDuration)/total) * time.Millisecond
+		return time.Duration(totalDuration/total) * time.Millisecond
 	}
 }
 
+// AddSuccessDuration for current percentile.
 func (c *Collector) AddSuccessDuration(d time.Duration) {
-	atomic.AddInt64(&c.successDuration, d.Milliseconds())
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.currentMetric().AddSuccessDuration(d)
+
 }
 
+// SuccessDuration of current percentile.
 func (c *Collector) SuccessDuration() (ms int64) {
-	return atomic.LoadInt64(&c.successDuration)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.currentMetric().SuccessDuration()
 }
 
+// AvgSuccessDuration of current percentile.
 func (c *Collector) AvgSuccessDuration() time.Duration {
-	success := atomic.LoadInt64(&c.success)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.currentMetric().AvgSuccessDuration()
+}
+
+// SummaryAvgSuccessDuration of all percentiles.
+func (c *Collector) SummaryAvgSuccessDuration() time.Duration {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	var success int64
+	var successDuration int64
+	for _, metric := range c.percentilesMetrics {
+		success += metric.Success()
+		successDuration += metric.SuccessDuration()
+	}
+
 	if success == 0 {
 		return time.Duration(0)
 	} else {
-		return time.Duration(atomic.LoadInt64(&c.successDuration)/success) * time.Millisecond
+		return time.Duration(successDuration/success) * time.Millisecond
 	}
 }
 
+// AddFailedDuration for current percentile.
 func (c *Collector) AddFailedDuration(d time.Duration) {
-	atomic.AddInt64(&c.failedDuration, d.Milliseconds())
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.currentMetric().AddFailedDuration(d)
 }
 
+// FailedDuration of current percentile.
 func (c *Collector) FailedDuration() (ms int64) {
-	return atomic.LoadInt64(&c.failedDuration)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.currentMetric().FailedDuration()
 }
 
+// AvgFailedDuration of current percentile.
 func (c *Collector) AvgFailedDuration() time.Duration {
-	failed := atomic.LoadInt64(&c.failed)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.currentMetric().AvgFailedDuration()
+}
+
+// SummaryAvgFailedDuration of all percentiles.
+func (c *Collector) SummaryAvgFailedDuration() time.Duration {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	var failed int64
+	var failedDuration int64
+	for _, metric := range c.percentilesMetrics {
+		failed += metric.Failed()
+		failedDuration += metric.FailedDuration()
+	}
+
 	if failed == 0 {
 		return time.Duration(0)
 	} else {
-		return time.Duration(atomic.LoadInt64(&c.failedDuration)/failed) * time.Millisecond
+		return time.Duration(failedDuration/failed) * time.Millisecond
 	}
 }
