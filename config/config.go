b0e17053fbe9b601571671584ee3ef3e35bf9fc2
Boris <Glazunov2142@icloud.com>
2023-12-24 14:55:38 +0300

    chore: improved cfg.

--- config/config.go
+++ config/config.go
@@ -6,6 +6,5 @@ type Config struct {
 	Percentiles int64  `arg:"-p,env:NUM_PERCENTILES" default:"4"`
 	MaxWorkers  int64  `arg:"-w,env:MAX_WORKERS"     default:"5"`
 	Duration    string `arg:"-d,env:DURATION"        default:"10m"`
-	StdErrFile  string `arg:"-e,env:STDERR_FILE"`
-	StdOutFile  string `arg:"-o,env:STDOUT_FILE"`
+	LogFile     string `arg:"-l,env:LOG_FILE"        default:"/dev/null"`
 }
