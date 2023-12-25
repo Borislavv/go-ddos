4e1a40e350c07e04b3c93a4200b3c2ffd78eac52
Boris <Glazunov2142@icloud.com>
2023-12-25 09:52:17 +0300

    chore: improved logs.

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"ddos/internal/ddos/app"
 	display "ddos/internal/display/app"
 	displayservice "ddos/internal/display/domain/service"
+	logservice "ddos/internal/log/domain/service"
 	stat "ddos/internal/stat/app"
 	statservice "ddos/internal/stat/domain/service"
 	"github.com/alexflint/go-arg"
@@ -30,8 +31,9 @@ func main() {
 		log.SetOutput(logfile)
 	}
 
+	logsCh := make(chan string, int64(cfg.MaxRPS)*cfg.MaxWorkers)
 	exitCh := make(chan os.Signal, 1)
-	defer close(exitCh)
+	defer func() { close(exitCh); close(logsCh) }()
 	signal.Notify(exitCh, os.Interrupt, syscall.SIGTERM)
 
 	dur, err := time.ParseDuration(cfg.Duration)
@@ -43,13 +45,15 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), dur)
 	defer wg.Wait()
 
+	lg := logservice.NewLogger(ctx, cfg, logsCh)
 	cl := statservice.NewCollector(cfg)
 	rd := displayservice.NewRenderer(ctx, cfg, exitCh)
 	st := stat.New(ctx, cfg, rd, cl)
 	di := display.New(ctx, rd)
-	dd := ddos.New(ctx, cfg, di, cl)
+	dd := ddos.New(ctx, cfg, di, lg, cl)
 
-	wg.Add(3)
+	wg.Add(4)
+	go lg.Run(wg)
 	go st.Run(wg)
 	go di.Run(wg)
 	go dd.Run(wg)
