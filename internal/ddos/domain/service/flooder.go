19c0cf01039abaa33ef94b8c75984e163aead2ed
Boris <Glazunov2142@icloud.com>
2023-12-25 16:25:50 +0300

    chore: improved flooder.

--- internal/ddos/domain/service/flooder.go
+++ internal/ddos/domain/service/flooder.go
@@ -20,11 +20,11 @@ type Flooder struct {
 	mu  *sync.RWMutex
 	ctx context.Context
 
-	logReqCh  chan *model.Response
-	cfg       *ddos.Config
-	display   *display.Display
-	logger    *logservice.Logger
-	collector *statservice.Collector
+	respProcCh chan *model.Response
+	cfg        *ddos.Config
+	display    *display.Display
+	logger     *logservice.Logger
+	collector  *statservice.Collector
 }
 
 func NewFlooder(
@@ -35,13 +35,13 @@ func NewFlooder(
 	collector *statservice.Collector,
 ) *Flooder {
 	return &Flooder{
-		mu:        &sync.RWMutex{},
-		logReqCh:  make(chan *model.Response, int64(cfg.MaxRPS)*cfg.MaxWorkers),
-		ctx:       ctx,
-		cfg:       cfg,
-		display:   display,
-		logger:    logger,
-		collector: collector,
+		mu:         &sync.RWMutex{},
+		respProcCh: make(chan *model.Response, int64(cfg.MaxRPS)*cfg.MaxWorkers),
+		ctx:        ctx,
+		cfg:        cfg,
+		display:    display,
+		logger:     logger,
+		collector:  collector,
 	}
 }
 
@@ -49,43 +49,47 @@ func (f *Flooder) Run() {
 	lwg := &sync.WaitGroup{}
 	defer lwg.Wait()
 	lwg.Add(1)
-	go func() {
-		defer lwg.Done()
-
-		for response := range f.logReqCh {
-			f.logFailedRequest(response)
-		}
-	}()
+	go f.handleResponsesProcessor(lwg)
+	defer close(f.respProcCh)
 
 	wg := &sync.WaitGroup{}
-	defer func() {
-		wg.Wait()
-		close(f.logReqCh)
-	}()
+	defer wg.Wait()
 	wg.Add(1)
-	go func() {
-		defer wg.Done()
-
-		requestSendTicker := time.NewTicker(time.Second / time.Duration(float64(f.cfg.MaxRPS)*1.10))
-		defer requestSendTicker.Stop()
-
-		threadSpawnTicker := time.NewTicker(time.Millisecond * 50)
-		defer threadSpawnTicker.Stop()
-
-		for {
-			f.collector.SetRPS()
-
-			select {
-			case <-f.ctx.Done():
-				return
-			case <-threadSpawnTicker.C:
-				f.spawnThread(wg, requestSendTicker, f.collector.RPS())
-			}
-		}
-	}()
+	go f.handleThreadsSpawner(wg, lwg)
 }
 
-func (f *Flooder) spawnThread(wg *sync.WaitGroup, requestSendTicker *time.Ticker, crps int64) {
+func (f *Flooder) handleThreadsSpawner(wg *sync.WaitGroup, lwg *sync.WaitGroup) {
+	defer wg.Done()
+
+	requestSendTicker := time.NewTicker(time.Second / time.Duration(float64(f.cfg.MaxRPS)*1.10))
+	defer requestSendTicker.Stop()
+
+	threadSpawnTicker := time.NewTicker(time.Millisecond * 100)
+	defer threadSpawnTicker.Stop()
+
+	for {
+		f.collector.SetRPS()
+
+		select {
+		case <-f.ctx.Done():
+			return
+		case <-threadSpawnTicker.C:
+			rps := f.collector.RPS()
+			f.spawnRequestSenderThread(wg, requestSendTicker, rps)
+			f.spawnResponseProcessorThread(lwg, rps)
+		}
+	}
+}
+
+func (f *Flooder) spawnResponseProcessorThread(wg *sync.WaitGroup, crps int64) {
+	if crps/3 > f.collector.Processors() && f.collector.Processors() < f.collector.Workers()*3 {
+		wg.Add(1)
+		go f.handleResponsesProcessor(wg)
+		f.collector.AddProcessor()
+	}
+}
+
+func (f *Flooder) spawnRequestSenderThread(wg *sync.WaitGroup, requestSendTicker *time.Ticker, crps int64) {
 	trps := int64(float64(f.cfg.MaxRPS) * 0.95)
 	if crps < trps && f.collector.Workers() < f.cfg.MaxWorkers {
 		wg.Add(1)
@@ -117,43 +121,41 @@ func (f *Flooder) sendRequest() {
 	if err != nil || resp.StatusCode != 200 {
 		f.collector.AddFailed()
 		f.collector.AddFailedDuration(time.Since(s))
-
-		f.logReqCh <- &model.Response{
-			Resp: resp,
-			Err:  err,
-		}
-
-		return
 	} else {
-		_, _ = io.Copy(io.Discard, resp.Body)
-		_ = resp.Body.Close()
+		f.collector.AddSuccess()
+		f.collector.AddSuccessDuration(time.Since(s))
 	}
 
-	f.collector.AddSuccess()
-	f.collector.AddSuccessDuration(time.Since(s))
+	f.sendResponseOnProcessing(resp, err)
 }
 
-func (f *Flooder) logFailedRequest(response *model.Response) {
-	defer func() { _ = response.Resp.Body.Close() }()
-
-	type Log struct {
-		URL        string              `json:"URL,omitempty"`
-		Error      string              `json:"error,omitempty"`
-		StatusCode int                 `json:"statusCode,omitempty"`
-		Headers    map[string][]string `json:"headers,omitempty"`
-		Data       map[string]string   `json:"data,omitempty"`
+func (f *Flooder) handleResponsesProcessor(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for response := range f.respProcCh {
+		f.processResponse(response)
 	}
+}
 
+func (f *Flooder) sendResponseOnProcessing(resp *http.Response, err error) {
+	f.respProcCh <- &model.Response{
+		Resp: resp,
+		Err:  err,
+	}
+}
+
+func (f *Flooder) processResponse(response *model.Response) {
 	if response.Err != nil {
-		bytes, merr := json.MarshalIndent(Log{Error: response.Err.Error()}, "", "  ")
+		bytes, merr := json.MarshalIndent(model.Log{Error: response.Err.Error()}, "", "  ")
 		if merr != nil {
 			f.logger.Println(merr.Error())
 			return
 		}
 
 		f.logger.Println(string(bytes))
-	} else {
-		l := Log{
+	} else if response.Resp.StatusCode != 200 {
+		defer func() { _ = response.Resp.Body.Close() }()
+
+		l := model.Log{
 			URL:        response.Resp.Request.URL.String(),
 			StatusCode: response.Resp.StatusCode,
 		}
@@ -166,14 +168,15 @@ func (f *Flooder) logFailedRequest(response *model.Response) {
 		}
 
 		if f.cfg.ResponseData != "" {
-			l.Data = make(map[string]string, 2)
 			bytes, rerr := io.ReadAll(response.Resp.Body)
 			if rerr != nil {
 				f.logger.Println(rerr.Error())
 				return
 			}
-			l.Data["expected"] = f.cfg.ResponseData
-			l.Data["gotten"] = string(bytes)
+			l.Data = model.Data{
+				Expected: f.cfg.ResponseData,
+				Gotten:   string(bytes),
+			}
 		}
 
 		bytes, merr := json.MarshalIndent(l, "", "  ")
@@ -183,5 +186,9 @@ func (f *Flooder) logFailedRequest(response *model.Response) {
 		}
 
 		f.logger.Println(string(bytes))
+	} else {
+		defer func() { _ = response.Resp.Body.Close() }()
+
+		_, _ = io.Copy(io.Discard, response.Resp.Body)
 	}
 }
