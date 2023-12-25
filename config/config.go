b670a6bd442211fa23a500a266a1e139d311c7ea
Boris <Glazunov2142@icloud.com>
2023-12-25 09:52:34 +0300

    chore: expanded and commented cfg.

--- config/config.go
+++ config/config.go
@@ -1,10 +1,21 @@
 package config
 
 type Config struct {
-	URL         string `arg:"-u,env:URL,required"`
-	MaxRPS      int    `arg:"-r,env:MAX_RPS"         default:"10"`
-	Percentiles int64  `arg:"-p,env:NUM_PERCENTILES" default:"4"`
-	MaxWorkers  int64  `arg:"-w,env:MAX_WORKERS"     default:"5"`
-	Duration    string `arg:"-d,env:DURATION"        default:"10m"`
-	LogFile     string `arg:"-l,env:LOG_FILE"        default:"/dev/null"`
+	// URL is a string with target DDOS url.
+	URL string `arg:"-u,env:URL,required"`
+	// ResponseData is string which contains expected response data.
+	// If it does not match, request will be marked as failed.
+	ResponseData string `arg:"-d,env:RESPONSE_DATA"`
+	// MaxRPS is a maximum number of requests per one second.
+	MaxRPS int `arg:"-r,env:MAX_RPS"                         default:"10"`
+	// MaxWorkers is a maximum workers number which will be spawn.
+	MaxWorkers int64 `arg:"-w,env:MAX_WORKERS"               default:"5"`
+	// Duration is a string which contains duration of the DDOS execution.
+	Duration string `arg:"-d,env:DURATION"                   default:"10m"`
+	// Percentiles is a number of parts by which will be separated output table.
+	Percentiles int64 `arg:"-p,env:NUM_PERCENTILES"          default:"1"`
+	// LogFile is a path to file into which will be redirected logs.
+	LogFile string `arg:"-l,env:LOG_FILE"                    default:"/dev/null"`
+	// LogHeaders is a slice of headers which must be caught on request error.
+	LogHeaders []string `arg:"-h,separate,env:CATCH_HEADERS"`
 }
