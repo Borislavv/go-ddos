92d9ee52850c3930506c834efa74d32524e5a775
Boris <Glazunov2142@icloud.com>
2023-12-26 10:27:03 +0300

    chore: renamed percentiles to stages.

--- config/config.go
+++ config/config.go
@@ -12,8 +12,8 @@ type Config struct {
 	MaxWorkers int64 `arg:"-w,env:MAX_WORKERS"               default:"5"`
 	// Duration is a string which contains duration of the DDOS execution.
 	Duration string `arg:"-d,env:DURATION"                   default:"10m"`
-	// Percentiles is a number of parts by which will be separated output table.
-	Percentiles int64 `arg:"-p,env:NUM_PERCENTILES"          default:"1"`
+	// Stages is a number of parts by which will be separated output table.
+	Stages int64 `arg:"-s,env:NUM_STAGES"                    default:"1"`
 	// LogFile is a path to file into which will be redirected logs.
 	LogFile string `arg:"-l,env:LOG_FILE"                    default:"/dev/null"`
 	// LogHeaders is a slice of headers which must be caught on request error.
