0fcd2781652e1b731a6de18b48b43cc9ed2539e7
Boris <Glazunov2142@icloud.com>
2023-12-29 12:00:00 +0300

    chore: new statuscode middleware.

--- internal/ddos/domain/service/sender/middleware/resp/statuscode.go
+++ internal/ddos/domain/service/sender/middleware/resp/statuscode.go
@@ -1,1 +1,25 @@
-package resp
+package respmiddleware
+
+import (
+	"ddos/internal/ddos/infrastructure/httpclient/middleware"
+	logservice "ddos/internal/log/domain/service"
+	"net/http"
+)
+
+type StatusCodeMiddleware struct {
+	logger *logservice.Logger
+}
+
+func NewStatusCodeMiddleware(logger *logservice.Logger) *StatusCodeMiddleware {
+	return &StatusCodeMiddleware{logger: logger}
+}
+
+func (m *StatusCodeMiddleware) HandleStatusCode(next httpclientmiddleware.ResponseHandler) httpclientmiddleware.ResponseHandler {
+	return httpclientmiddleware.ResponseHandlerFunc(func(resp *http.Response, err error) (*http.Response, error) {
+		if err != nil && resp.StatusCode != http.StatusOK {
+			m.logger.Printf("request failed, received status code %d", resp.StatusCode)
+		}
+
+		return next.Handle(resp, err)
+	})
+}
