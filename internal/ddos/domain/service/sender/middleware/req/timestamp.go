cf1e1d06e88bd93ce937bc5c72b6f27cadf75baf
Boris <Glazunov2142@icloud.com>
2023-12-29 12:00:00 +0300

    chore: new timestamp middleware.

--- internal/ddos/domain/service/sender/middleware/req/timestamp.go
+++ internal/ddos/domain/service/sender/middleware/req/timestamp.go
@@ -1,21 +1,22 @@
-package req
+package reqmiddleware
 
 import (
 	"ddos/internal/ddos/infrastructure/httpclient/middleware"
+	"fmt"
 	"net/http"
-	"strconv"
 	"time"
 )
 
-const timestamp = "timestamp"
+const Timestamp = "Timestamp"
 
 func AddTimestamp(next httpclientmiddleware.RequestModifier) httpclientmiddleware.RequestModifier {
 	return httpclientmiddleware.RequestModifierFunc(func(req *http.Request) (*http.Response, error) {
 		copyValues := req.URL.Query()
-		if copyValues.Has(timestamp) {
-			copyValues.Del(timestamp)
+		if copyValues.Has(Timestamp) {
+			copyValues.Del(Timestamp)
 		}
-		copyValues.Add(timestamp, strconv.Itoa(time.Now().Nanosecond()))
+		//copyValues.Add(Timestamp, strconv.FormatInt(time.Now().UnixMilli(), 10))
+		copyValues.Add(Timestamp, fmt.Sprintf("%d", time.Now().UnixMilli()))
 		req.URL.RawQuery = copyValues.Encode()
 		return next.Do(req)
 	})
