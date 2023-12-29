02069e0ddccd1d0be60a08ab4f16c1e666ec5d60
Boris <Glazunov2142@icloud.com>
2023-12-29 12:00:00 +0300

    chore: new pooled http client interface.

--- internal/ddos/infrastructure/httpclient/pooled_interface.go
+++ internal/ddos/infrastructure/httpclient/pooled_interface.go
@@ -1,12 +1,12 @@
 package httpclient
 
 import (
-	"ddos/internal/ddos/infrastructure/http/middleware"
+	"ddos/internal/ddos/infrastructure/httpclient/middleware"
 	"net/http"
 )
 
 type Pooled interface {
 	Do(req *http.Request) (*http.Response, error)
-	OneReq(middlewares ...middleware.RequestMiddlewareFunc) Pooled
-	OneResp(middlewares ...middleware.ResponseMiddlewareFunc) Pooled
+	OnReq(middlewares ...httpclientmiddleware.RequestMiddlewareFunc) Pooled
+	OnResp(middlewares ...httpclientmiddleware.ResponseMiddlewareFunc) Pooled
 }
