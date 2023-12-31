072a733490777c327de64790f7fd93a971b265fd
Boris <Glazunov2142@icloud.com>
2023-12-31 08:53:57 +0300

    fix: fixed close by max workers strategy.

--- internal/ddos/domain/service/balancer/req/voter/close/bymaxworkers.go
+++ internal/ddos/domain/service/balancer/req/voter/close/bymaxworkers.go
@@ -5,8 +5,8 @@ import (
 	statservice "ddos/internal/stat/domain/service"
 )
 
-func ByMaxWorkers(cfg *config.Config, collector *statservice.Collector) func(cfg *config.Config, collector *statservice.Collector) bool {
+func ByMaxWorkers() func(cfg *config.Config, collector *statservice.Collector) bool {
 	return func(cfg *config.Config, collector *statservice.Collector) bool {
-		return false
+		return collector.Workers() > cfg.MaxWorkers
 	}
 }
