accf0b9416525759398d712bc5096d21bc732dbb
Boris <Glazunov2142@icloud.com>
2023-12-31 08:52:05 +0300

    chore: TECH DEBT into the randurl middleware.

--- internal/ddos/domain/service/sender/middleware/req/randurl.go
+++ internal/ddos/domain/service/sender/middleware/req/randurl.go
@@ -2,26 +2,37 @@ package reqmiddleware
 
 import (
 	"ddos/internal/ddos/infrastructure/httpclient/middleware"
-	"errors"
+	logservice "ddos/internal/log/domain/service"
 	"math/rand"
 	"net/http"
 	"net/url"
 	"time"
 )
 
-var URLs = []string{}
+type RandUrlMiddleware struct {
+	URLs   []string
+	logger *logservice.Logger
+}
 
-func RandUrl(next httpclientmiddleware.RequestModifier) httpclientmiddleware.RequestModifier {
+func NewRandUrlMiddleware(URLs []string, logger *logservice.Logger) *RandUrlMiddleware {
+	return &RandUrlMiddleware{URLs: URLs, logger: logger}
+}
+
+func (m *RandUrlMiddleware) AddRandUrl(next httpclientmiddleware.RequestModifier) httpclientmiddleware.RequestModifier {
 	return httpclientmiddleware.RequestModifierFunc(func(req *http.Request) (*http.Response, error) {
-		rand.Seed(time.Now().UnixNano())
+		if req != nil {
+			rand.Seed(time.Now().UnixNano())
 
-		if len(URLs) != 0 {
-			u := URLs[rand.Intn(len(URLs)+1)]
-			p, err := url.Parse(u)
-			if err != nil {
-				return nil, errors.New("unable to parse given url, " + err.Error())
+			if len(m.URLs) != 0 {
+				//u := m.URLs[rand.Intn(len(m.URLs)+1)]
+				u := m.URLs[0] // TODO fix it
+				p, err := url.Parse(u)
+				if err != nil {
+					m.logger.Println("unable to parse given url, " + err.Error())
+					return next.Do(req)
+				}
+				req.URL = p
 			}
-			req.URL = p
 		}
 
 		return next.Do(req)
