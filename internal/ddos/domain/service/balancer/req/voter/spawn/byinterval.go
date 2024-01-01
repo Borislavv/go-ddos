3bcc2ad72f37ab8dffda247a2346740e63d17ff9
Boris <Glazunov2142@icloud.com>
2024-01-01 11:00:00 +0300

    chore: injected collector interface into byinterval voter.

--- internal/ddos/domain/service/balancer/req/voter/spawn/byinterval.go
+++ internal/ddos/domain/service/balancer/req/voter/spawn/byinterval.go
@@ -5,8 +5,8 @@ import (
 	statservice "ddos/internal/stat/domain/service"
 )
 
-func ByInterval(cfg *config.Config, collector *statservice.Collector) func(cfg *config.Config, collector *statservice.Collector) bool {
-	return func(cfg *config.Config, collector *statservice.Collector) bool {
+func ByInterval() func(cfg *config.Config, collector statservice.Collector) bool {
+	return func(cfg *config.Config, collector statservice.Collector) bool {
 		return false
 	}
 }
