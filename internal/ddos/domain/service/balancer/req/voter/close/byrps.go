e6ce71141940eb3457d6740fcb740869e8f06847
Boris <Glazunov2142@icloud.com>
2024-01-01 11:00:00 +0300

    chore: injected collector interface into byrps voter.

--- internal/ddos/domain/service/balancer/req/voter/close/byrps.go
+++ internal/ddos/domain/service/balancer/req/voter/close/byrps.go
@@ -5,8 +5,8 @@ import (
 	statservice "ddos/internal/stat/domain/service"
 )
 
-func ByRPS(cfg *config.Config, collector *statservice.Collector) func(cfg *config.Config, collector *statservice.Collector) bool {
-	return func(cfg *config.Config, collector *statservice.Collector) bool {
+func ByRPS() func(cfg *config.Config, collector statservice.Collector) bool {
+	return func(cfg *config.Config, collector statservice.Collector) bool {
 		return false
 	}
 }
