3be203393bfca69b9f2635c3dfd6792fe80c6738
Boris <Glazunov2142@icloud.com>
2024-01-01 11:00:00 +0300

    chore: implemented collector interface.

--- internal/stat/domain/service/collector_interface.go
+++ internal/stat/domain/service/collector_interface.go
@@ -1,1 +1,52 @@
 package statservice
+
+import (
+	"ddos/internal/stat/domain/model"
+	"sync"
+	"time"
+)
+
+type Collector interface {
+	Run(wg *sync.WaitGroup)
+
+	Metric(stage int64) (metric *model.Metrics, found bool)
+	Stages() int64
+	SummaryDuration() time.Duration
+	SummaryRPS() int64
+
+	AddWorker()
+	Workers() int64
+	RemoveWorker()
+
+	AddTotalRequest()
+	TotalRequests() int64
+	SummaryTotalRequests() int64
+
+	AddSuccessRequest()
+	SuccessRequests() int64
+	SummarySuccessRequests() int64
+
+	AddFailedRequest()
+	FailedRequests() int64
+	SummaryFailedRequests() int64
+
+	AddTotalRequestsDuration(d time.Duration)
+	TotalRequestsDuration() (ms int64)
+	AvgTotalRequestsDuration() time.Duration
+	SummaryAvgTotalRequestsDuration() time.Duration
+
+	AddSuccessRequestsDuration(d time.Duration)
+	SuccessRequestsDuration() (ms int64)
+	AvgSuccessRequestsDuration() time.Duration
+	SummaryAvgSuccessRequestsDuration() time.Duration
+
+	AddFailedRequestsDuration(d time.Duration)
+	FailedRequestsDuration() (ms int64)
+	AvgFailedRequestsDuration() time.Duration
+	SummaryAvgFailedRequestsDuration() time.Duration
+
+	HttpClientPoolBusy() int64
+	SetHttpClientPoolBusy()
+	HttpClientPoolTotal() int64
+	SetHttpClientPoolTotal()
+}
