888a3aa37fe0adffca4085f0eaf80b4116872079
Boris <Glazunov2142@icloud.com>
2024-01-01 11:00:00 +0300

    chore: injected collector interface into byminworkers voter.

--- internal/ddos/domain/service/balancer/req/voter/spawn/byminworkers.go
+++ internal/ddos/domain/service/balancer/req/voter/spawn/byminworkers.go
@@ -5,8 +5,8 @@ import (
 	statservice "ddos/internal/stat/domain/service"
 )
 
-func ByMinWorkers() func(cfg *config.Config, collector *statservice.Collector) bool {
-	return func(cfg *config.Config, collector *statservice.Collector) bool {
+func ByMinWorkers() func(cfg *config.Config, collector statservice.Collector) bool {
+	return func(cfg *config.Config, collector statservice.Collector) bool {
 		return cfg.MaxWorkers > collector.Workers()
 	}
 }
