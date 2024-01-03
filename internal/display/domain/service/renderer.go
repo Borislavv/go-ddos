92a52887deebd8eea23dda11692a0c1ec8ae0689
Boris <Glazunov2142@icloud.com>
2024-01-03 15:43:00 +0300

    chore: improved concurrency into the renderer.

--- internal/display/domain/service/renderer.go
+++ internal/display/domain/service/renderer.go
@@ -4,41 +4,48 @@ import (
 	"context"
 	"ddos/config"
 	displaymodel "ddos/internal/display/domain/model"
+	logservice "ddos/internal/log/domain/service"
 	"github.com/nsf/termbox-go"
 	"github.com/olekukonko/tablewriter"
-	"log"
 	"os"
 	"sync"
 )
 
-type Renderer struct {
-	ctx       context.Context
-	cfg       *config.Config
-	dataCh    chan *displaymodel.Table
-	summaryCh chan *displaymodel.Table
-	exitCh    chan<- os.Signal
-	stopCh    chan struct{}
+type RendererService struct {
+	ctx            context.Context
+	logger         logservice.Logger
+	tableCh        chan *displaymodel.Table
+	summaryTableCh chan *displaymodel.Table
+	exitCh         chan<- os.Signal
+	stopCh         chan struct{}
+	isClosed       int64
 }
 
-func NewRenderer(ctx context.Context, cfg *config.Config, exitCh chan<- os.Signal) *Renderer {
-	return &Renderer{
-		ctx:       ctx,
-		exitCh:    exitCh,
-		dataCh:    make(chan *displaymodel.Table, cfg.MaxRPS),
-		summaryCh: make(chan *displaymodel.Table),
-		stopCh:    make(chan struct{}, 1),
+func NewRenderer(
+	ctx context.Context,
+	cfg *config.Config,
+	logger logservice.Logger,
+	exitCh chan<- os.Signal,
+) *RendererService {
+	return &RendererService{
+		ctx:            ctx,
+		exitCh:         exitCh,
+		logger:         logger,
+		tableCh:        make(chan *displaymodel.Table, cfg.MaxWorkers),
+		summaryTableCh: make(chan *displaymodel.Table, 1),
+		stopCh:         make(chan struct{}, 1),
 	}
 }
 
-func (r *Renderer) SendData(data *displaymodel.Table) {
-	r.dataCh <- data
+func (r *RendererService) TableCh() chan<- *displaymodel.Table {
+	return r.tableCh
 }
 
-func (r *Renderer) SendSummary(data *displaymodel.Table) {
-	r.summaryCh <- data
+func (r *RendererService) SummaryTableCh() chan<- *displaymodel.Table {
+	return r.summaryTableCh
 }
 
-func (r *Renderer) Close(wg *sync.WaitGroup) {
+func (r *RendererService) Listen(wg *sync.WaitGroup) {
 	defer func() {
 		close(r.stopCh)
 		wg.Done()
@@ -49,28 +56,24 @@ func (r *Renderer) Close(wg *sync.WaitGroup) {
 		case <-r.ctx.Done():
 			return
 		default:
-			switch ev := termbox.PollEvent(); ev.Type {
+			switch event := termbox.PollEvent(); event.Type {
 			case termbox.EventKey:
-				if ev.Key == termbox.KeyCtrlC || ev.Key == termbox.KeyCtrlZ {
+				if event.Key == termbox.KeyCtrlC || event.Key == termbox.KeyCtrlZ {
 					return
 				}
 			case termbox.EventError:
-				panic(ev.Err)
+				r.logger.Println(event.Err.Error())
 			}
 		}
 	}
 }
 
-func (r *Renderer) Draw(wg *sync.WaitGroup) {
-	defer func() {
-		close(r.dataCh)
-		close(r.summaryCh)
-		wg.Done()
-	}()
+func (r *RendererService) Draw(wg *sync.WaitGroup) {
+	defer wg.Done()
 
-	err := termbox.Init()
-	if err != nil {
-		panic(err)
+	if err := termbox.Init(); err != nil {
+		r.logger.Println(err.Error())
+		return
 	}
 	defer termbox.Close()
 
@@ -84,33 +87,47 @@ func (r *Renderer) Draw(wg *sync.WaitGroup) {
 
 			r.exitCh <- os.Interrupt
 
-			data := <-r.summaryCh
+			data := <-r.summaryTableCh
 
 			// reinitialized the table as the summary
 			table = tablewriter.NewWriter(os.Stdout)
 			table.SetAlignment(tablewriter.ALIGN_CENTER)
 
 			// draw the summary table
-			r.draw(table, data)
+			if err := r.draw(table, data); err != nil {
+				r.logger.Println(err.Error())
+				continue
+			}
 
 			return
-		case data := <-r.dataCh:
+		case data := <-r.tableCh:
 			// clear a terminal window
-			if err = termbox.Clear(termbox.ColorDefault, termbox.ColorDefault); err != nil {
-				log.Fatalln(err)
+			if err := termbox.Clear(termbox.ColorDefault, termbox.ColorDefault); err != nil {
+				r.logger.Println(err.Error())
+				continue
 			}
 			// sync a terminal window
-			if err = termbox.Sync(); err != nil {
-				return
+			if err := termbox.Sync(); err != nil {
+				r.logger.Println(err.Error())
+				continue
 			}
 
 			// draw a table
-			r.draw(table, data)
+			if err := r.draw(table, data); err != nil {
+				r.logger.Println(err.Error())
+				continue
+			}
 		}
 	}
 }
 
-func (r *Renderer) draw(table *tablewriter.Table, data *displaymodel.Table) {
+func (r *RendererService) Close() error {
+	close(r.tableCh)
+	close(r.summaryTableCh)
+	return nil
+}
+
+func (r *RendererService) draw(table *tablewriter.Table, data *displaymodel.Table) error {
 	// set up a table header
 	table.SetHeader(data.Header)
 
@@ -129,6 +146,8 @@ func (r *Renderer) draw(table *tablewriter.Table, data *displaymodel.Table) {
 
 	// draw a table
 	if err := termbox.Flush(); err != nil {
-		log.Fatalln(err)
+		return err
 	}
+
+	return nil
 }
