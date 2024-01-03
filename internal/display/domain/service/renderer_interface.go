2164ff82830eaced930969875132c381872571e8
Boris <Glazunov2142@icloud.com>
2024-01-03 15:43:00 +0300

    chore: added renderer interface.

--- internal/display/domain/service/renderer_interface.go
+++ internal/display/domain/service/renderer_interface.go
@@ -1,1 +1,14 @@
 package displayservice
+
+import (
+	displaymodel "ddos/internal/display/domain/model"
+	"sync"
+)
+
+type Renderer interface {
+	Draw(wg *sync.WaitGroup)
+	Listen(wg *sync.WaitGroup)
+	TableCh() chan<- *displaymodel.Table
+	SummaryTableCh() chan<- *displaymodel.Table
+	Close() error
+}
