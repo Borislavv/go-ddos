fb333582e1f2527f036d50c07a43d62fa1468e7d
Boris <Glazunov2142@icloud.com>
2024-01-03 12:00:00 +0300

    chore: display service.

--- internal/display/app/display.go
+++ internal/display/app/display.go
@@ -3,30 +3,39 @@ package display
 import (
 	"context"
 	displayservice "ddos/internal/display/domain/service"
+	logservice "ddos/internal/log/domain/service"
 	"sync"
 )
 
-type Display struct {
+type App struct {
 	ctx      context.Context
+	logger   logservice.Logger
 	renderer displayservice.Renderer
 }
 
 func New(
 	ctx context.Context,
+	logger logservice.Logger,
 	renderer displayservice.Renderer,
-) *Display {
-	return &Display{
+) *App {
+	return &App{
 		ctx:      ctx,
+		logger:   logger,
 		renderer: renderer,
 	}
 }
 
-func (d *Display) Run(mwg *sync.WaitGroup) {
-	defer mwg.Done()
+func (d *App) Run(mwg *sync.WaitGroup) {
+	defer func() {
+		d.logger.Println("display.App.Run() is closed")
+		mwg.Done()
+	}()
+
+	ctx, cancel := context.WithCancel(d.ctx)
 
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
-	go d.renderer.Draw(wg)
-	go d.renderer.Listen(wg)
+	go d.renderer.Draw(wg, ctx)
+	go d.renderer.Listen(wg, cancel)
 	wg.Wait()
 }
