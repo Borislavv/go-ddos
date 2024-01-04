11d01e30c26570b55b51b45eb7ce373d03313ddc
Boris <Glazunov2142@icloud.com>
2024-01-04 08:47:05 +0300

    chore: improved statscode middleware.

--- internal/flooder/domain/service/sender/middleware/resp/statuscode.go
+++ internal/flooder/domain/service/sender/middleware/resp/statuscode.go
@@ -1,8 +1,12 @@
 package respmiddleware
 
 import (
+	"encoding/json"
+	"fmt"
+	floodermodel "github.com/Borislavv/go-ddos/internal/flooder/domain/model"
 	middleware "github.com/Borislavv/go-ddos/internal/flooder/infrastructure/httpclient/middleware"
 	logservice "github.com/Borislavv/go-ddos/internal/log/domain/service"
+	"io"
 	"net/http"
 )
 
@@ -17,7 +21,24 @@ func NewStatusCodeMiddleware(logger logservice.Logger) *StatusCodeMiddleware {
 func (m *StatusCodeMiddleware) HandleStatusCode(next middleware.ResponseHandler) middleware.ResponseHandler {
 	return middleware.ResponseHandlerFunc(func(resp *http.Response, err error) (*http.Response, error) {
 		if err == nil && resp != nil && resp.StatusCode != http.StatusOK {
-			m.logger.Printfln("request failed, received status code %d", resp.StatusCode)
+			var msg string
+			if resp.StatusCode == http.StatusInternalServerError && resp.Body != nil {
+				b, e := io.ReadAll(resp.Body)
+				if e != nil {
+					m.logger.Println(e.Error())
+					return next.Handle(resp, err)
+				}
+				msg = string(b)
+			} else {
+				msg = fmt.Sprintf("request failed, received status code %d", resp.StatusCode)
+			}
+
+			b, e := json.MarshalIndent(floodermodel.Log{Error: msg}, "", "  ")
+			if e != nil {
+				m.logger.Println(e.Error())
+				return next.Handle(resp, err)
+			}
+			m.logger.Println(string(b))
 		}
 
 		return next.Handle(resp, err)
