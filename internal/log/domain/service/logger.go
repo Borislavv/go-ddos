dc0c5e5373db049c86671f93b00cf047df5d214e
Boris <Glazunov2142@icloud.com>
2024-01-04 12:00:00 +0300

    chore: async logger.

--- internal/log/domain/service/logger.go
+++ internal/log/domain/service/logger.go
@@ -5,8 +5,10 @@ import (
 	"ddos/config"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"sync"
+	"time"
 )
 
 type CloseFunc func()
@@ -18,9 +20,14 @@ type Async struct {
 }
 
 func NewAsync(ctx context.Context, cfg *config.Config) *Async {
+	buff := int64(math.Ceil(float64(cfg.MaxRPS) / float64(cfg.MaxWorkers)))
+	if buff <= 0 {
+		buff = 1
+	}
+
 	l := &Async{
 		ctx: ctx,
-		ch:  make(chan string, cfg.MaxRPS*int(cfg.MaxWorkers)),
+		ch:  make(chan string, buff),
 	}
 
 	if cfg.LogFile != "" {
@@ -38,29 +45,13 @@ func NewAsync(ctx context.Context, cfg *config.Config) *Async {
 func (l *Async) Run(wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	for {
-		select {
-		case <-l.ctx.Done():
-			go func() {
-				for range l.ch {
-					// clean remain messages into the chan
-					// due to non-block other goroutines
-				}
-			}()
-			return
-		case msg := <-l.ch:
-			log.Println(msg)
-		}
+	for msg := range l.ch {
+		log.Println(msg)
 	}
 }
 
 func (l *Async) Println(msg string) {
-	select {
-	case l.ch <- msg:
-	case <-l.ctx.Done():
-	default:
-		fmt.Println(msg)
-	}
+	l.ch <- msg
 }
 
 func (l *Async) Printf(msg string, args ...any) {
@@ -68,11 +59,18 @@ func (l *Async) Printf(msg string, args ...any) {
 }
 
 func (l *Async) Close() error {
+	l.Println("logger.Async.Run() is closed")
+	if len(l.ch) > 0 {
+		t := time.NewTimer(time.Second * 3)
+		defer t.Stop()
+		<-t.C
+	}
+	close(l.ch)
 	if l.fd != nil {
 		if err := l.fd.Close(); err != nil {
 			return err
 		}
 	}
-	close(l.ch)
+	log.SetOutput(os.Stdout)
 	return nil
 }
