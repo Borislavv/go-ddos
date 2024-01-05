8de901fd9924ddddb34438507afd56ecea0b9f98
Boris <Glazunov2142@icloud.com>
2024-01-05 09:13:20 +0300

    chore: improved voter interface.

--- internal/flooder/domain/service/workers/voter/voter_interface.go
+++ internal/flooder/domain/service/workers/voter/voter_interface.go
@@ -2,8 +2,9 @@ package voter
 
 import (
 	"github.com/Borislavv/go-ddos/internal/flooder/domain/enum"
+	"time"
 )
 
 type Voter interface {
-	Vote() (weight enum.Weight)
+	Vote() (weight enum.Weight, sleep time.Duration)
 }
