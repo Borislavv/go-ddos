ec2556c59abe088e9864a8159a6066177b20c25d
Boris <Glazunov2142@icloud.com>
2024-01-05 09:11:50 +0300

    chore: improved atleastone vote strategy.

--- internal/flooder/domain/service/workers/vote/strategy/atleastone.go
+++ internal/flooder/domain/service/workers/vote/strategy/atleastone.go
@@ -3,6 +3,7 @@ package votestrategy
 import (
 	"github.com/Borislavv/go-ddos/internal/flooder/domain/enum"
 	"github.com/Borislavv/go-ddos/internal/flooder/domain/service/workers/voter"
+	"time"
 )
 
 type AtLeastOneVoter struct {
@@ -20,28 +21,32 @@ func NewAtLeastOneVoter(
 	}
 }
 
-func (s *AtLeastOneVoter) For() enum.Action {
+func (v *AtLeastOneVoter) For() (action enum.Action, sleep time.Duration) {
+	var slpSpawn time.Duration
 	var forSpawn enum.Weight
-	for _, v := range s.spawnVoters {
-		w := v.Vote()
+	for _, c := range v.spawnVoters {
+		w, s := c.Vote()
 		if forSpawn < w {
 			forSpawn = w
+			slpSpawn = s
 		}
 	}
 
+	var slpClose time.Duration
 	var forClose enum.Weight
-	for _, v := range s.closeVoters {
-		w := v.Vote()
+	for _, c := range v.closeVoters {
+		w, s := c.Vote()
 		if forClose < w {
 			forClose = w
+			slpClose = s
 		}
 	}
 
 	if forSpawn > forClose {
-		return enum.Spawn
+		return enum.Spawn, slpSpawn
 	} else if forClose > forSpawn {
-		return enum.Close
+		return enum.Close, slpClose
 	} else {
-		return enum.Await
+		return enum.Await, slpSpawn + slpClose
 	}
 }
