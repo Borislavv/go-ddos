cad34ce9a1ad503b68420ece3c97307f6e27f54e
Boris <Glazunov2142@icloud.com>
2024-01-06 11:26:45 +0300

    chore: RENDERER RESOLVED.

--- internal/display/domain/service/rendererv2.go
+++ internal/display/domain/service/rendererv2.go
@@ -2,11 +2,17 @@ package displayservice
 
 import (
 	"context"
+	"fmt"
 	statservice "github.com/Borislavv/go-ddos/internal/stat/domain/service"
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
+	"github.com/nsf/termbox-go"
+	"github.com/olekukonko/tablewriter"
 	"log"
+	"math"
 	"os"
+	"runtime"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -15,6 +21,7 @@ const (
 	rps        = 0
 	durSuccess = 0
 	durFailed  = 1
+	goroutines = 0
 )
 
 type RendererV2Service struct {
@@ -25,6 +32,7 @@ type RendererV2Service struct {
 	log *widgets.List
 	dur *widgets.Plot
 	rps *widgets.Plot
+	grt *widgets.Plot
 }
 
 func NewRendererV2Service(
@@ -42,6 +50,12 @@ func NewRendererV2Service(
 func (r *RendererV2Service) Run(wg *sync.WaitGroup) {
 	defer wg.Done()
 
+	defer func() {
+		if err := r.summary(); err != nil {
+			log.Println(err)
+		}
+	}()
+
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
 	}
@@ -50,56 +64,73 @@ func (r *RendererV2Service) Run(wg *sync.WaitGroup) {
 	width, height := ui.TerminalDimensions()
 
 	r.dur = r.initDurPlot(width, height)
-	r.rps = r.initRpsPlot(width, height)
 	r.log = r.initLogsList(width, height)
+	r.grt = r.initGoroutinesPlot(width, height)
+	r.rps = r.initRpsPlot(width, height)
 
-	ticker := time.NewTicker(time.Millisecond * 250)
+	ticker := time.NewTicker(time.Millisecond * 100)
 	defer ticker.Stop()
 
+	eventCh := ui.PollEvents()
 	for {
 		select {
 		case <-r.ctx.Done():
 			return
-		case e := <-ui.PollEvents():
+		case e := <-eventCh:
 			switch e.ID {
 			case "<C-c>", "<C-z>":
 				r.exitCh <- os.Interrupt
 				return
 			case "<Resize>":
 				payload := e.Payload.(ui.Resize)
-				r.rps.SetRect(0, 0, payload.Width, payload.Height-30)
-				r.dur.SetRect(0, payload.Height-30, payload.Width, payload.Height-10)
-				r.log.SetRect(0, payload.Height-10, payload.Width, payload.Height)
+				width, height = payload.Width, payload.Height
+
+				r.rps.SetRect(
+					0,
+					0,
+					int(math.Round((float64(width)/100)*60)),
+					int(math.Round((float64(height)/100)*40)),
+				)
+
+				r.dur.SetRect(
+					0,
+					int(math.Round((float64(height)/100)*80)),
+					int(math.Round((float64(width)/100)*60)),
+					int(math.Round((float64(height)/100)*40)),
+				)
+
+				r.grt.SetRect(
+					int(math.Round((float64(width)/100)*60)),
+					0,
+					width,
+					int(math.Round((float64(height)/100)*20)),
+				)
+
+				r.log.SetRect(
+					0,
+					int(math.Round((float64(height)/100)*80)),
+					width,
+					height,
+				)
+
 				ui.Clear()
 			}
 		case <-ticker.C:
 			// avg rps number
-			if r.isMaxLenReached(width, r.rps.Data[rps]) {
-				r.rps.Data[rps] = r.reduceDataSlice(r.rps.Data[rps])
+			if r.isMaxLenReachedForMainPlots(width, r.rps.Data[rps]) {
+				r.rps.Data[rps] = r.rps.Data[rps][1:]
 			}
-			p := float64(r.collector.RPS())
-			if p == 0 {
-				r.rps.LineColors[rps] = ui.ColorBlack
-			} else {
-				r.rps.LineColors[rps] = ui.ColorGreen
-			}
-			r.rps.Data[rps] = append(r.rps.Data[rps], p)
+			r.rps.Data[rps] = append(r.rps.Data[rps], float64(r.collector.RPS()))
 
 			// avg success requests duration
-			if r.isMaxLenReached(width, r.dur.Data[durSuccess]) {
-				r.dur.Data[durSuccess] = r.reduceDataSlice(r.dur.Data[durSuccess])
+			if r.isMaxLenReachedForMainPlots(width, r.dur.Data[durSuccess]) {
+				r.dur.Data[durSuccess] = r.dur.Data[durSuccess][1:]
 			}
-			s := float64(r.collector.AvgSuccessRequestsDuration().Milliseconds())
-			if s == 0 {
-				r.dur.LineColors[durSuccess] = ui.ColorBlack
-			} else {
-				r.dur.LineColors[durSuccess] = ui.ColorGreen
-			}
-			r.dur.Data[durSuccess] = append(r.dur.Data[durSuccess], s)
+			r.dur.Data[durSuccess] = append(r.dur.Data[durSuccess], float64(r.collector.AvgSuccessRequestsDuration().Milliseconds()))
 
 			// avg failed requests duration
-			if r.isMaxLenReached(width, r.dur.Data[durFailed]) {
-				r.dur.Data[durFailed] = r.reduceDataSlice(r.dur.Data[durFailed])
+			if r.isMaxLenReachedForMainPlots(width, r.dur.Data[durFailed]) {
+				r.dur.Data[durFailed] = r.dur.Data[durFailed][1:]
 			}
 			f := float64(r.collector.AvgFailedRequestsDuration().Milliseconds())
 			if f == 0 {
@@ -108,18 +139,24 @@ func (r *RendererV2Service) Run(wg *sync.WaitGroup) {
 				r.dur.LineColors[durFailed] = ui.ColorRed
 			}
 			r.dur.Data[durFailed] = append(r.dur.Data[durFailed], f)
-			
-			ui.Render(r.rps, r.dur, r.log)
+
+			// number of goroutines
+			if r.isMaxLenReachedForMinorPlots(width, r.grt.Data[goroutines]) {
+				r.grt.Data[goroutines] = r.grt.Data[goroutines][1:]
+			}
+			r.grt.Data[goroutines] = append(r.grt.Data[goroutines], float64(runtime.NumGoroutine()))
+
+			ui.Render(r.rps, r.dur, r.grt, r.log)
 		}
 	}
 }
 
-func (r *RendererV2Service) isMaxLenReached(width int, data []float64) bool {
-	return len(data) >= ((width / 100) * 98)
+func (r *RendererV2Service) isMaxLenReachedForMinorPlots(width int, data []float64) bool {
+	return len(data) >= int(math.Round((float64(width)/100)*38))
 }
 
-func (r *RendererV2Service) reduceDataSlice(old []float64) (new []float64) {
-	return old[1:]
+func (r *RendererV2Service) isMaxLenReachedForMainPlots(width int, data []float64) bool {
+	return len(data) >= int(math.Round((float64(width)/100)*57))
 }
 
 func (r *RendererV2Service) Write(p []byte) (n int, err error) {
@@ -140,11 +177,16 @@ func (r *RendererV2Service) initRpsPlot(width, height int) *widgets.Plot {
 	plot.AxesColor = ui.ColorWhite
 
 	plot.Data = make([][]float64, 1)
-	plot.Data[rps] = make([]float64, 0, width)
+	plot.Data[rps] = make([]float64, 0, int(math.Round((float64(width)/100)*60)))
 	plot.Data[rps] = append(plot.Data[rps], 0)
 	plot.LineColors[rps] = ui.ColorGreen
 
-	plot.SetRect(0, 0, width, height-30)
+	plot.SetRect(
+		0,
+		0,
+		int(math.Round((float64(width)/100)*60)),
+		int(math.Round((float64(height)/100)*40)),
+	)
 
 	return plot
 }
@@ -156,8 +198,8 @@ func (r *RendererV2Service) initDurPlot(width, height int) *widgets.Plot {
 
 	plot.Data = make([][]float64, 2)
 
-	plot.Data[durSuccess] = make([]float64, 0, width)
-	plot.Data[durFailed] = make([]float64, 0, width)
+	plot.Data[durSuccess] = make([]float64, 0, (width/100)*60)
+	plot.Data[durFailed] = make([]float64, 0, (width/100)*60)
 
 	plot.Data[durSuccess] = append(plot.Data[durSuccess], 0)
 	plot.Data[durFailed] = append(plot.Data[durFailed], 0)
@@ -165,7 +207,32 @@ func (r *RendererV2Service) initDurPlot(width, height int) *widgets.Plot {
 	plot.LineColors[durSuccess] = ui.ColorGreen
 	plot.LineColors[durFailed] = ui.ColorRed
 
-	plot.SetRect(0, height-30, width, height-10)
+	plot.SetRect(
+		0,
+		int(math.Round((float64(height)/100)*80)),
+		int(math.Round((float64(width)/100)*60)),
+		int(math.Round((float64(height)/100)*40)),
+	)
+
+	return plot
+}
+
+func (r *RendererV2Service) initGoroutinesPlot(width, height int) *widgets.Plot {
+	plot := widgets.NewPlot()
+	plot.Title = "Goroutines"
+	plot.AxesColor = ui.ColorWhite
+
+	plot.Data = make([][]float64, 1)
+	plot.Data[goroutines] = make([]float64, 0, (width/100)*40)
+	plot.Data[goroutines] = append(plot.Data[goroutines], 0)
+	plot.LineColors[goroutines] = ui.ColorGreen
+
+	plot.SetRect(
+		int(math.Round((float64(width)/100)*60)),
+		0,
+		width,
+		int(math.Round((float64(height)/100)*20)),
+	)
 
 	return plot
 }
@@ -177,6 +244,66 @@ func (r *RendererV2Service) initLogsList(width, height int) *widgets.List {
 	logs.Rows = make([]string, 0, 10)
 
 	logs.TextStyle = ui.NewStyle(ui.ColorYellow)
-	logs.SetRect(0, height-10, width, height)
+	logs.SetRect(
+		0,
+		int(math.Round((float64(height)/100)*80)),
+		width,
+		height,
+	)
 	return logs
+}
+
+func (r *RendererV2Service) summary() error {
+	// reinitialized the table as the summary
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetAlignment(tablewriter.ALIGN_CENTER)
+
+	// set up a table header
+	table.SetHeader([]string{
+		"duration",
+		"rps",
+		"workers",
+		"total reqs.",
+		"success reqs.",
+		"failed reqs.",
+		"avg. total reqs. dur.",
+		"avg. success reqs. dur.",
+		"avg. failed reqs. dur.",
+		"http pool",
+		"goroutines",
+	})
+
+	// clear a table rows
+	table.ClearRows()
+
+	var outside string
+	if r.collector.HttpClientOutOfPool() > 0 {
+		outside = fmt.Sprintf(" (OUTSIDE: %d)", r.collector.HttpClientOutOfPool())
+	}
+
+	// set up a table rows
+	table.Append([]string{
+		r.collector.SummaryDuration().String(),
+		strconv.FormatInt(r.collector.SummaryRPS(), 10),
+		strconv.FormatInt(r.collector.Workers(), 10),
+		strconv.FormatInt(r.collector.SummaryTotalRequests(), 10),
+		strconv.FormatInt(r.collector.SummarySuccessRequests(), 10),
+		strconv.FormatInt(r.collector.SummaryFailedRequests(), 10),
+		r.collector.SummaryAvgTotalRequestsDuration().String(),
+		r.collector.SummaryAvgSuccessRequestsDuration().String(),
+		r.collector.SummaryAvgFailedRequestsDuration().String(),
+		strconv.FormatInt(r.collector.HttpClientPoolBusy(), 10) + " / " +
+			strconv.FormatInt(r.collector.HttpClientPoolTotal(), 10) + outside,
+		strconv.Itoa(runtime.NumGoroutine()),
+	})
+
+	// render a table
+	table.Render()
+
+	// draw a table
+	if err := termbox.Flush(); err != nil {
+		return err
+	}
+
+	return nil
 }
