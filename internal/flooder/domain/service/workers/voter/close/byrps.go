250ee2d1981895fdaf5b51109fca5af48e1302d0
Boris <Glazunov2142@icloud.com>
2024-01-07 15:45:00 +0300

    chore: updated byrps close voter.

--- internal/flooder/domain/service/workers/voter/close/byrps.go
+++ internal/flooder/domain/service/workers/voter/close/byrps.go
@@ -2,11 +2,31 @@ package balancerclosevoter
 
 import (
 	"github.com/Borislavv/go-ddos/config"
+	"github.com/Borislavv/go-ddos/internal/flooder/domain/enum"
 	statservice "github.com/Borislavv/go-ddos/internal/stat/domain/service"
+	"time"
 )
 
-func ByRPS() func(cfg *config.Config, collector statservice.Collector) bool {
-	return func(cfg *config.Config, collector statservice.Collector) bool {
-		return false
+type ByRPS struct {
+	cfg       *config.Config
+	collector statservice.Collector
+}
+
+func NewByRPS(cfg *config.Config, collector statservice.Collector) *ByRPS {
+	collector.SetLastCloseByRPS()
+
+	return &ByRPS{
+		cfg:       cfg,
+		collector: collector,
 	}
+}
+
+func (s *ByRPS) Vote() (isFor bool, weight enum.Weight) {
+	if time.Since(s.collector.LastCloseByRPS()) < s.cfg.SpawnIntervalValue {
+		return false, enum.Check
+	}
+
+	defer s.collector.SetLastCloseByRPS()
+	return s.collector.RPS() > int64(float64(s.cfg.TargetRPS)*(1+s.cfg.ToleranceCoefficient)) &&
+		s.collector.Workers() > s.cfg.MinWorkers, enum.For
 }
