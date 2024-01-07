784656c26631e7a626bbb99cee9ac5d6eda1c446
Boris <Glazunov2142@icloud.com>
2024-01-07 15:45:00 +0300

    chore: updated all vote strategy.

--- internal/flooder/domain/service/workers/vote/strategy/all.go
+++ internal/flooder/domain/service/workers/vote/strategy/all.go
@@ -1,32 +1,21 @@
 package votestrategy
 
 import (
-	"github.com/Borislavv/go-ddos/config"
 	"github.com/Borislavv/go-ddos/internal/flooder/domain/service/workers/voter"
-	statservice "github.com/Borislavv/go-ddos/internal/stat/domain/service"
 )
 
 type AllVoters struct {
-	voters    []voter.Voter
-	cfg       *config.Config
-	collector statservice.Collector
+	voters []voter.Voter
 }
 
-func NewAllVoters(
-	voters []voter.Voter,
-	cfg *config.Config,
-	collector statservice.Collector,
-) *AllVoters {
-	return &AllVoters{
-		voters:    voters,
-		cfg:       cfg,
-		collector: collector,
-	}
+func NewAllVoters(voters []voter.Voter) *AllVoters {
+	return &AllVoters{voters: voters}
 }
 
 func (s *AllVoters) IsFor() bool {
-	for _, voter := range s.voters {
-		if !voter(s.cfg, s.collector) {
+	for _, v := range s.voters {
+		isFor, _ := v.Vote()
+		if !isFor {
 			return false
 		}
 	}
