284360a67a6e9fb20095fe06d6c36d5bdd5d2c57
Boris <Glazunov2142@icloud.com>
2024-01-07 15:45:00 +0300

    chore: updated byaavgduration spawn voter.

--- internal/flooder/domain/service/workers/voter/spawn/byavgduration.go
+++ internal/flooder/domain/service/workers/voter/spawn/byavgduration.go
@@ -2,11 +2,32 @@ package balancerspawnvoter
 
 import (
 	"github.com/Borislavv/go-ddos/config"
+	"github.com/Borislavv/go-ddos/internal/flooder/domain/enum"
 	statservice "github.com/Borislavv/go-ddos/internal/stat/domain/service"
+	"time"
 )
 
-func ByAvgDuration() func(cfg *config.Config, collector statservice.Collector) bool {
-	return func(cfg *config.Config, collector statservice.Collector) bool {
-		return false
+type ByAvgDuration struct {
+	cfg       *config.Config
+	collector statservice.Collector
+}
+
+func NewByAvgDuration(cfg *config.Config, collector statservice.Collector) *ByAvgDuration {
+	collector.SetLastSpawnByAvgDuration()
+
+	return &ByAvgDuration{
+		cfg:       cfg,
+		collector: collector,
 	}
+}
+
+func (s *ByAvgDuration) Vote() (isFor bool, weight enum.Weight) {
+	if time.Since(s.collector.LastSpawnByAvgDuration()) < s.cfg.SpawnIntervalValue {
+		return false, enum.Check
+	}
+
+	defer s.collector.SetLastSpawnByAvgDuration()
+	return s.collector.AvgSuccessRequestsDuration() <
+		time.Duration(float64(s.cfg.TargetAvgSuccessRequestsDurationValue.Nanoseconds())*(1-s.cfg.ToleranceCoefficient)) &&
+		s.collector.Workers() < s.cfg.MaxWorkers, enum.SureFor
 }
