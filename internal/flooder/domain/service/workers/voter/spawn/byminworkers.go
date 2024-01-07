456c079eb47d397d922bed26c09e8054b8dc34ce
Boris <Glazunov2142@icloud.com>
2024-01-07 15:45:00 +0300

    chore: updated byminworkers spawn voter.

--- internal/flooder/domain/service/workers/voter/spawn/byminworkers.go
+++ internal/flooder/domain/service/workers/voter/spawn/byminworkers.go
@@ -2,11 +2,22 @@ package balancerspawnvoter
 
 import (
 	"github.com/Borislavv/go-ddos/config"
+	"github.com/Borislavv/go-ddos/internal/flooder/domain/enum"
 	statservice "github.com/Borislavv/go-ddos/internal/stat/domain/service"
 )
 
-func ByMinWorkers() func(cfg *config.Config, collector statservice.Collector) bool {
-	return func(cfg *config.Config, collector statservice.Collector) bool {
-		return cfg.MinWorkers > collector.Workers()
+type ByMinWorkers struct {
+	cfg       *config.Config
+	collector statservice.Collector
+}
+
+func NewByMinWorkers(cfg *config.Config, collector statservice.Collector) *ByMinWorkers {
+	return &ByMinWorkers{
+		cfg:       cfg,
+		collector: collector,
 	}
+}
+
+func (s *ByMinWorkers) Vote() (isFor bool, weight enum.Weight) {
+	return s.cfg.MinWorkers > s.collector.Workers(), enum.TotallyFor
 }
