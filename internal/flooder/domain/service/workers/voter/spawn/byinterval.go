e49f4640e234cb0ae8c1e8f6a9d6bfe46c6c33f5
Boris <Glazunov2142@icloud.com>
2024-01-07 15:45:00 +0300

    chore: updated byinterval spawn voter.

--- internal/flooder/domain/service/workers/voter/spawn/byinterval.go
+++ internal/flooder/domain/service/workers/voter/spawn/byinterval.go
@@ -2,11 +2,27 @@ package balancerspawnvoter
 
 import (
 	"github.com/Borislavv/go-ddos/config"
+	"github.com/Borislavv/go-ddos/internal/flooder/domain/enum"
 	statservice "github.com/Borislavv/go-ddos/internal/stat/domain/service"
+	"time"
 )
 
-func ByInterval() func(cfg *config.Config, collector statservice.Collector) bool {
-	return func(cfg *config.Config, collector statservice.Collector) bool {
-		return false
+type ByInterval struct {
+	cfg       *config.Config
+	collector statservice.Collector
+}
+
+func NewByInterval(cfg *config.Config, collector statservice.Collector) *ByInterval {
+	collector.SetLastSpawnByInterval()
+
+	return &ByInterval{
+		cfg:       cfg,
+		collector: collector,
 	}
+}
+
+func (s *ByInterval) Vote() (isFor bool, weight enum.Weight) {
+	defer s.collector.SetLastSpawnByInterval()
+	return time.Since(s.collector.LastSpawnByInterval()) > s.cfg.SpawnIntervalValue &&
+		s.collector.Workers() < s.cfg.MaxWorkers, enum.TotallyFor
 }
