f27cda576ad8dfdcd796557312be1f5bec13adad
Boris <Glazunov2142@icloud.com>
2024-01-07 15:45:00 +0300

    chore: updated bymaxworkers close voter.

--- internal/flooder/domain/service/workers/voter/close/bymaxworkers.go
+++ internal/flooder/domain/service/workers/voter/close/bymaxworkers.go
@@ -2,11 +2,22 @@ package balancerclosevoter
 
 import (
 	"github.com/Borislavv/go-ddos/config"
+	"github.com/Borislavv/go-ddos/internal/flooder/domain/enum"
 	statservice "github.com/Borislavv/go-ddos/internal/stat/domain/service"
 )
 
-func ByMaxWorkers() func(cfg *config.Config, collector statservice.Collector) bool {
-	return func(cfg *config.Config, collector statservice.Collector) bool {
-		return collector.Workers() > cfg.MaxWorkers
+type ByMaxWorkers struct {
+	cfg       *config.Config
+	collector statservice.Collector
+}
+
+func NewByMaxWorkers(cfg *config.Config, collector statservice.Collector) *ByMaxWorkers {
+	return &ByMaxWorkers{
+		cfg:       cfg,
+		collector: collector,
 	}
+}
+
+func (s *ByMaxWorkers) Vote() (isFor bool, weight enum.Weight) {
+	return s.collector.Workers() > s.cfg.MaxWorkers, enum.TotallyFor
 }
