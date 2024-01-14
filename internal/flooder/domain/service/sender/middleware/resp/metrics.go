ff470432aab15a1a41a010755e37524860bb9253
Boris <Glazunov2142@icloud.com>
2024-01-14 15:47:00 +0300

    chore: changes into the response metrics middlware.

--- internal/flooder/domain/service/sender/middleware/resp/metrics.go
+++ internal/flooder/domain/service/sender/middleware/resp/metrics.go
@@ -1,12 +1,10 @@
 package respmiddleware
 
 import (
-	reqmiddleware "github.com/Borislavv/go-ddos/internal/flooder/domain/service/sender/middleware/req"
 	middleware "github.com/Borislavv/go-ddos/internal/flooder/infrastructure/httpclient/middleware"
 	logservice "github.com/Borislavv/go-ddos/internal/log/domain/service"
 	statservice "github.com/Borislavv/go-ddos/internal/stat/domain/service"
 	"net/http"
-	"strconv"
 	"time"
 )
 
@@ -26,42 +24,19 @@ func NewMetricsMiddleware(
 }
 
 func (m *MetricsMiddleware) CollectMetrics(next middleware.ResponseHandler) middleware.ResponseHandler {
-	return middleware.ResponseHandlerFunc(func(resp *http.Response, err error) (*http.Response, error) {
-		var duration time.Duration
+	return middleware.ResponseHandlerFunc(func(resp *http.Response, err error, timestamp int64) (*http.Response, error, int64) {
+		duration := time.Since(time.UnixMilli(timestamp))
 
-		defer func() {
-			m.collector.AddTotalRequest()
-			m.collector.AddTotalRequestsDuration(duration)
-			if err != nil || (resp != nil && resp.StatusCode != http.StatusOK) {
-				m.collector.AddFailedRequest()
-				m.collector.AddFailedRequestsDuration(duration)
-			} else {
-				m.collector.AddSuccessRequest()
-				m.collector.AddSuccessRequestsDuration(duration)
-			}
-		}()
-
-		if resp != nil && resp.Request != nil && resp.Request.URL != nil {
-			copyValues := resp.Request.URL.Query()
-			if !copyValues.Has(reqmiddleware.Timestamp) {
-				m.logger.Println("timestamp doesn't exists in the request query, unable to determine request duration")
-				return next.Handle(resp, err)
-			}
-			timestampStr := copyValues.Get(reqmiddleware.Timestamp)
-			if timestampStr == "" {
-				m.logger.Println("timestamp value is empty, unable to determine request duration")
-				return next.Handle(resp, err)
-			}
-			timestamp, perr := strconv.ParseInt(timestampStr, 10, 64)
-			if perr != nil {
-				m.logger.Println("timestamp cannot by cast to int, unable to determine request duration")
-				return next.Handle(resp, err)
-			}
-			duration = time.Since(time.UnixMilli(timestamp))
+		m.collector.AddTotalRequest()
+		m.collector.AddTotalRequestsDuration(duration)
+		if err != nil || (resp != nil && resp.StatusCode != http.StatusOK) {
+			m.collector.AddFailedRequest()
+			m.collector.AddFailedRequestsDuration(duration)
 		} else {
-			duration = time.Duration(0)
+			m.collector.AddSuccessRequest()
+			m.collector.AddSuccessRequestsDuration(duration)
 		}
 
-		return next.Handle(resp, err)
+		return next.Handle(resp, err, timestamp)
 	})
 }
