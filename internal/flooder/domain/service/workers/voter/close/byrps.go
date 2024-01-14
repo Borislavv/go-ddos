d91d3901a6f06ff6fedcaae6dc96911a679a2c85
Boris <Glazunov2142@icloud.com>
2024-01-14 11:13:35 +0300

    chore: improved byrps close voter.

--- internal/flooder/domain/service/workers/voter/close/byrps.go
+++ internal/flooder/domain/service/workers/voter/close/byrps.go
@@ -10,23 +10,40 @@ import (
 type ByRPS struct {
 	cfg       *config.Config
 	collector statservice.Collector
+	rpsTrend  []int64
 }
 
 func NewByRPS(cfg *config.Config, collector statservice.Collector) *ByRPS {
 	return &ByRPS{
 		cfg:       cfg,
 		collector: collector,
+		rpsTrend:  make([]int64, 0, 100),
 	}
 }
 
 func (s *ByRPS) Vote() (weight enum.Weight, sleep time.Duration) {
-	if s.collector.RPS() > int64(float64(s.cfg.TargetRPS)*(1+s.cfg.ToleranceCoefficient)) {
+	currentRps := s.collector.RPS()
+
+	if len(s.rpsTrend) == cap(s.rpsTrend) {
+		s.rpsTrend = s.rpsTrend[10:]
+	}
+	s.rpsTrend = append(s.rpsTrend, currentRps)
+
+	if currentRps > int64(float64(s.cfg.TargetRPS)*(1+s.cfg.ToleranceCoefficient)) {
 		if s.collector.Workers() > s.cfg.MinWorkers {
-			return enum.SureFor, time.Millisecond * 250
+			return enum.SureFor, s.cfg.SpawnIntervalValue * 2
 		} else {
-			return enum.For, time.Millisecond * 500
+			return enum.Check, 0
+		}
+	} else {
+		isFor := false
+		if len(s.rpsTrend) == cap(s.rpsTrend) {
+			isFor = s.rpsTrend[0] < s.rpsTrend[len(s.rpsTrend)-1]
+		}
+		if isFor {
+			return enum.AbsolutelyFor, s.cfg.SpawnIntervalValue / 4
 		}
 	}
 
-	return enum.Check, 0
+	return enum.Check, s.cfg.SpawnIntervalValue * 2
 }
