b511a54df0abf45aa4a8b6701d7399d1ef420dbb
Boris <Glazunov2142@icloud.com>
2024-01-16 11:56:40 +0300

    chore: improved closebody middleware.

--- internal/flooder/domain/service/sender/middleware/resp/closebody.go
+++ internal/flooder/domain/service/sender/middleware/resp/closebody.go
@@ -1,8 +1,8 @@
 package respmiddleware
 
 import (
-	floodermodel "github.com/Borislavv/go-ddos/internal/flooder/domain/model"
 	middleware "github.com/Borislavv/go-ddos/internal/flooder/infrastructure/httpclient/middleware"
+	httpclientmodel "github.com/Borislavv/go-ddos/internal/flooder/infrastructure/httpclient/model"
 	logservice "github.com/Borislavv/go-ddos/internal/log/domain/service"
 )
 
@@ -15,7 +15,7 @@ func NewCloseBodyMiddleware(logger logservice.Logger) *CloseBodyMiddleware {
 }
 
 func (m *CloseBodyMiddleware) CloseResponseBody(next middleware.ResponseHandler) middleware.ResponseHandler {
-	return middleware.ResponseHandlerFunc(func(resp *floodermodel.Response) *floodermodel.Response {
+	return middleware.ResponseHandlerFunc(func(resp *httpclientmodel.Response) *httpclientmodel.Response {
 		if !resp.IsFailed() && resp.Resp().Body != nil {
 			if err := resp.Resp().Body.Close(); err != nil {
 				m.logger.Println(err.Error())
