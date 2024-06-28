a258b609740cc7f8d84c5eee6a98c6519c40766d
Boris <glazunov2142@icloud.com>
2024-06-28 17:08:52 +0300

    feat: renamed ini request middleware.

---
+++ internal/flooder/domain/service/sender/middleware/req/init_request.go
@@ -0,0 +1,25 @@
+package reqmiddleware
+
+import (
+	middleware "github.com/Borislavv/go-ddos/internal/flooder/infrastructure/httpclient/middleware"
+	logservice "github.com/Borislavv/go-ddos/internal/log/domain/service"
+	"net/http"
+)
+
+type InitRequestMiddleware struct {
+	logger logservice.Logger
+}
+
+func NewInitRequestMiddleware(logger logservice.Logger) *InitRequestMiddleware {
+	return &InitRequestMiddleware{logger: logger}
+}
+
+func (m *InitRequestMiddleware) InitRequest(next middleware.RequestModifier) middleware.RequestModifier {
+	return middleware.RequestModifierFunc(func(req *http.Request) (*http.Response, error) {
+		req, err := http.NewRequest("GET", "", nil)
+		if err != nil {
+			return nil, err
+		}
+		return next.Do(req)
+	})
+}
