5f991d02f88960fc61acc933482f49c145c699e0
Boris <glazunov2142@icloud.com>
2024-06-28 17:09:07 +0300

    feat: moved rand url middleware.

---
+++ internal/flooder/domain/service/sender/middleware/req/rand_url.go
@@ -0,0 +1,44 @@
+package reqmiddleware
+
+import (
+	"github.com/Borislavv/go-ddos/internal/flooder/infrastructure/httpclient/middleware"
+	logservice "github.com/Borislavv/go-ddos/internal/log/domain/service"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type RandUrlMiddleware struct {
+	urls   []string
+	logger logservice.Logger
+}
+
+func NewRandUrlMiddleware(URLs []string, logger logservice.Logger) *RandUrlMiddleware {
+	return &RandUrlMiddleware{urls: URLs, logger: logger}
+}
+
+func (m *RandUrlMiddleware) AddRandUrl(next httpclientmiddleware.RequestModifier) httpclientmiddleware.RequestModifier {
+	return httpclientmiddleware.RequestModifierFunc(func(req *http.Request) (*http.Response, error) {
+		if req != nil {
+			random := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+			if len(m.urls) != 0 {
+				var u *url.URL
+				var err error
+				if len(m.urls) > 1 {
+					u, err = url.Parse(m.urls[random.Intn(len(m.urls))])
+				} else {
+					u, err = url.Parse(m.urls[0])
+				}
+				if err != nil {
+					m.logger.Println("unable to parse given url, " + err.Error())
+					return next.Do(req)
+				}
+				req.URL = u
+			}
+		}
+
+		return next.Do(req)
+	})
+}
