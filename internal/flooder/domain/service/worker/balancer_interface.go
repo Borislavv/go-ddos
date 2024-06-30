4ab18e12f29b58cc64fa0406b3fef109f85c8b81
Boris <glazunov2142@icloud.com>
2024-06-30 13:17:57 +0300

    feat: improved balancer interface.

---
+++ internal/flooder/domain/service/worker/balancer_interface.go
@@ -0,0 +1,10 @@
+package worker
+
+import (
+	"github.com/Borislavv/go-ddos/internal/flooder/domain/enum"
+	"time"
+)
+
+type Balancer interface {
+	CurrentAction() (action enum.Action, sleep time.Duration)
+}
