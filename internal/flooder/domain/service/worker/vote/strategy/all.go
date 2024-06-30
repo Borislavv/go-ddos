437aad5754c832c6e470e7716489347923aae144
Boris <glazunov2142@icloud.com>
2024-06-30 13:18:27 +0300

    feat: improved all strategy.

---
+++ internal/flooder/domain/service/worker/vote/strategy/all.go
@@ -0,0 +1,60 @@
+package votestrategy
+
+import (
+	"github.com/Borislavv/go-ddos/internal/flooder/domain/enum"
+	"github.com/Borislavv/go-ddos/internal/flooder/domain/service/worker/voter"
+	"time"
+)
+
+type AllVoters struct {
+	spawnVoters []voter.Voter
+	closeVoters []voter.Voter
+}
+
+func NewAllVoters(
+	spawnVoters []voter.Voter,
+	closeVoters []voter.Voter,
+) *AllVoters {
+	return &AllVoters{
+		spawnVoters: spawnVoters,
+		closeVoters: closeVoters,
+	}
+}
+
+func (v *AllVoters) For() (action enum.Action, sleep time.Duration) {
+	var forSpawn int
+	var slpSpawn time.Duration
+	for _, c := range v.spawnVoters {
+		w, s := c.Vote()
+		if w == enum.Check {
+			continue
+		}
+		forSpawn++
+		slpSpawn += s
+	}
+	if forSpawn < len(v.spawnVoters) {
+		forSpawn = enum.Check
+	}
+
+	var forClose int
+	var slpClose time.Duration
+	for _, c := range v.closeVoters {
+		w, s := c.Vote()
+		if w == enum.Check {
+			continue
+		}
+		forClose++
+		slpClose += s
+	}
+	if forClose < len(v.closeVoters) {
+		forClose = enum.Check
+	}
+
+	if forSpawn != enum.Check && forClose != enum.Check {
+		return enum.Await, slpClose + slpSpawn
+	} else if forSpawn != enum.Check {
+		return enum.Spawn, slpSpawn
+	} else {
+		return enum.Close, slpClose
+	}
+}
