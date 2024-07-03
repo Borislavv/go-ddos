40cbe97b587246a0a319aa136f72a093f906cdbc
Boris <glazunov2142@icloud.com>
2024-07-03 14:04:42 +0300

    fix: request init_middleare.

--- internal/flooder/domain/service/sender/middleware/req/init_request.go
+++ internal/flooder/domain/service/sender/middleware/req/init_request.go
@@ -1,22 +1,24 @@
 package reqmiddleware
 
 import (
+	"context"
 	middleware "github.com/Borislavv/go-ddos/internal/flooder/infrastructure/httpclient/middleware"
 	logservice "github.com/Borislavv/go-ddos/internal/log/domain/service"
 	"net/http"
 )
 
 type InitRequestMiddleware struct {
+	ctx    context.Context
 	logger logservice.Logger
 }
 
-func NewInitRequestMiddleware(logger logservice.Logger) *InitRequestMiddleware {
-	return &InitRequestMiddleware{logger: logger}
+func NewInitRequestMiddleware(ctx context.Context, logger logservice.Logger) *InitRequestMiddleware {
+	return &InitRequestMiddleware{ctx: ctx, logger: logger}
 }
 
 func (m *InitRequestMiddleware) InitRequest(next middleware.RequestModifier) middleware.RequestModifier {
 	return middleware.RequestModifierFunc(func(req *http.Request) (*http.Response, error) {
-		req, err := http.NewRequest("GET", "", nil)
+		req, err := http.NewRequestWithContext(m.ctx, "GET", "", nil)
 		if err != nil {
 			return nil, err
 		}
